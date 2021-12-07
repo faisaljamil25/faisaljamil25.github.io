3ec50728f02462c92b93e98c3696cfc5f09b092d
Faisal Jamil <faisaljamilcs@gmail.com>
2021-12-07 20:37:10 +0530

    feat: theme setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+            rel='stylesheet'
+          ></link>
           <link rel='icon' type='image/svg+xml' href='/favicon.ico'></link>
           <link rel='alternate icon' href='/favicon.ico' />
           <meta name='theme-color' content='#ffffff'></meta>
