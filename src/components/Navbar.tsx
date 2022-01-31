ff9d35e6d824ae928be401b39e8e16de14d1c756
Faisal Jamil <faisaljamilcs@gmail.com>
2022-01-31 16:52:25 +0530

    feat: Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,43 @@
+import { useTheme } from 'next-themes';
+import React from 'react';
+import { BsMoon } from 'react-icons/bs';
+import { HiSun } from 'react-icons/hi';
+import { useIsMounted } from '@/hooks/useIsMounted';
+import ThemeToggleButton from '@/components/ThemeToggleButton';
+
 const Navbar = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const isMounted = useIsMounted();
+
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <header className=' nav'>
+      <nav className='flex items-center justify-between flex-1 max-w-[1920px]'>
+        <div>
+          <h4 className='text-current text-black no-underline dark:text-white hover:text-current'>
+            <span className='font-bold '>Faisal Jamil</span>
+          </h4>
+        </div>
+        <div>
+          {isMounted &&
+            (resolvedTheme === 'light' ? (
+              <ThemeToggleButton
+                onClick={() => setTheme('dark')}
+                aria-label='Dark mode toggle'
+              >
+                <BsMoon size='1.25rem' />
+              </ThemeToggleButton>
+            ) : (
+              <ThemeToggleButton onClick={() => setTheme('light')}>
+                <HiSun
+                  size='1.25rem'
+                  className='text-white'
+                  aria-label='Light mode toggle'
+                />
+              </ThemeToggleButton>
+            ))}
+        </div>
+      </nav>
+    </header>
   );
 };
 
