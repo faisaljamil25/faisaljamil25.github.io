a1e966333a4bb24d38cb855cc00791a37dcea438
Faisal Jamil <faisaljamilcs@gmail.com>
2022-01-31 17:59:50 +0530

    feat: deploy to github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  reactStrictMode: true,
-}
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  assetPrefix: !debug ? '' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    return config;
+  },
+};
