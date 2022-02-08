cb5d64bdd88277472d1e1b25557f0eeaf1b81ba9
Faisal Jamil <faisaljamilcs@gmail.com>
2022-02-08 17:52:16 +0530

    feat: projects

---
+++ src/components/Projects.tsx
@@ -0,0 +1,28 @@
+import { projects } from 'data';
+import React from 'react';
+import ProjectCard from './ProjectCard';
+import SectionTitle from './SectionTitle';
+
+const Projects = () => {
+  return (
+    <section>
+      <SectionTitle subtitle='Here are some of my best works.'>
+        Projects
+      </SectionTitle>
+      <div className='grid grid-cols-1 gap-x-[140px] gap-y-20 pt-10 lg:grid-cols-2 '>
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            imgSrc={project.imgSrc}
+            title={project.title}
+            description={project.description}
+            liveLink={project.liveLink}
+            repoLink={project.repoLink}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import MainLayout from '../layouts/MainLayout';
 import Landing from '@/components/Landing';
+import Projects from '@/components/Projects';
 
 const Home: NextPage = () => {
   return (
     <MainLayout>
       <Landing />
+      <Projects />
     </MainLayout>
   );
 };
