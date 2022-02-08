200687321391a092c81da6110943a08edde1bf09
Faisal Jamil <faisaljamilcs@gmail.com>
2022-02-08 19:36:28 +0530

    feat: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { DefaultSeo } from 'next-seo';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
+      <DefaultSeo
+        title='Faisal Jamil'
+        description='Fullstack Web Developer'
+        openGraph={{
+          type: 'website',
+          description: 'Fullstack Web Developer',
+          title: 'Faisal Jamil',
+          site_name: 'Faisal Jamil',
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
