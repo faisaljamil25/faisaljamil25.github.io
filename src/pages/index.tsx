6a77116cfb55a7c75d15e4098a03218531c9dcca
Faisal Jamil <faisaljamilcs@gmail.com>
2022-02-08 20:52:16 +0530

    feat: social links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 import MainLayout from '../layouts/MainLayout';
 import Landing from '@/components/Landing';
 import Projects from '@/components/Projects';
+import SocialLinks from '@/components/SocialLinks';
 
 const Home: NextPage = () => {
   return (
     <MainLayout>
       <Landing />
       <Projects />
+      <SocialLinks />
     </MainLayout>
   );
 };
