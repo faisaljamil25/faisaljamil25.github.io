7c36472bde2942dfde751c2ddc65e1ea0b01d5e9
Faisal Jamil <faisaljamilcs@gmail.com>
2022-02-08 19:09:53 +0530

    chore: projects data

---
+++ src/data.ts
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'ChimeraX',
+    description:
+      'A web application platform to conduct online quizzing contests built with Next.JS, TypeScript & Express.js',
+    imgSrc: '/chimerax.png',
+    liveLink: 'https://chimerax.istemanit.in/',
+    repoLink: 'https://github.com/ISTE-SC-MANIT/chimerax22-ui',
+  },
+  {
+    id: 2,
+    title: 'Memories',
+    description: 'Image sharing web app built with React and Express.js',
+    imgSrc: '/memories.png',
+    liveLink: 'https://snap-memories.netlify.app/',
+    repoLink: 'https://github.com/faisaljamil25/memories-client',
+  },
+  {
+    id: 3,
+    title: 'Ignite Games',
+    description: 'Website to search video games information',
+    imgSrc: '/ignite.png',
+    liveLink: 'https://rog-ignite.netlify.app/',
+    repoLink: 'https://github.com/faisaljamil25/ignite',
+  },
+  {
+    id: 4,
+    title: 'WAVES MUSIC PLAYER',
+    description: 'A music player built with React.js',
+    imgSrc: '/music-player.png',
+    liveLink: 'https://waves-musics-player.netlify.app/',
+    repoLink: 'https://github.com/faisaljamil25/music-player',
+  },
+];
