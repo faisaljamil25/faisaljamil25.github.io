be3ce11d97cb3b2b4289be7e3afe109b0e5b202c
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-29 22:11:04 +0530

    fix: skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,17 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
-import SectionTitle from './SectionTitle';
-import icons from '../socials';
 import { useTheme } from 'next-themes';
+import SectionTitle from './SectionTitle';
+import icons from '../icons';
+import { Tooltip } from './Tooltip';
 
 const Skills = () => {
   const { theme } = useTheme();
   return (
     <section>
-      <SectionTitle subtitle='Here are some of my best works.'>
-        Skills
-      </SectionTitle>
-      <div className='px-6 2xl:px-0 w-full flex justify-center items-center py-10 md:py-16 '>
+      <SectionTitle subtitle="Technologies I've worked on">Skills</SectionTitle>
+      <div className='px-6 2xl:px-0 w-full flex justify-center items-center py-10 md:py-14 '>
         <div className='mx-auto container'>
           <div>
             <div className='container mx-auto grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 pt-6 gap-10 justify-center'>
@@ -33,27 +32,29 @@ const Skills = () => {
                 icons.postman,
                 icons.solidity,
                 icons.ethereum,
-              ].map((image, index) => (
+              ].map((icon, index) => (
                 <div
                   className='pt-6 lg:pt-0 flex justify-center'
                   key={`image ${index}`}
                 >
-                  <Image
-                    src={image}
-                    key={index}
-                    objectFit='contain'
-                    width={50}
-                    height={50}
-                    alt='social'
-                    className={
-                      theme === 'dark' &&
-                      (image === icons.nextjs ||
-                        image === icons.express ||
-                        image === icons.solidity)
-                        ? 'filter invert'
-                        : undefined
-                    }
-                  />
+                  <Tooltip tooltipText={icon.name}>
+                    <Image
+                      src={icon.path}
+                      key={index}
+                      objectFit='contain'
+                      width={50}
+                      height={50}
+                      alt='social'
+                      className={`cursor-pointer ${
+                        theme === 'dark' &&
+                        (icon === icons.nextjs ||
+                          icon === icons.express ||
+                          icon === icons.solidity)
+                          ? 'filter invert'
+                          : undefined
+                      }`}
+                    />
+                  </Tooltip>
                 </div>
               ))}
             </div>
---
+++ src/icons.ts
@@ -0,0 +1,88 @@
+const html = {
+  path: '/icons/html5.svg',
+  name: 'HTML',
+};
+const css = {
+  path: '/icons/css3.svg',
+  name: 'CSS',
+};
+const js = {
+  path: '/icons/javascript.svg',
+  name: 'JavaScript',
+};
+const mui = {
+  path: '/icons/material-ui.svg',
+  name: 'Material UI',
+};
+const ts = {
+  path: '/icons/typescript.svg',
+  name: 'TypeScript',
+};
+const git = {
+  path: '/icons/git.svg',
+  name: 'Git',
+};
+const figma = {
+  path: '/icons/figma.svg',
+  name: 'Figma',
+};
+const react = {
+  path: '/icons/react.svg',
+  name: 'React',
+};
+const nextjs = {
+  path: '/icons/next-js.svg',
+  name: 'Next.js',
+};
+const nodejs = {
+  path: '/icons/nodejs.svg',
+  name: 'Node.js',
+};
+const express = {
+  path: '/icons/expressjs.svg',
+  name: 'Express.js',
+};
+const tailwind = {
+  path: '/icons/tailwindcss.svg',
+  name: 'Tailwind',
+};
+const mongodb = {
+  path: '/icons/mongodb.svg',
+  name: 'MongoDb',
+};
+const firebase = {
+  path: '/icons/firebase.svg',
+  name: 'Firebase',
+};
+const postman = {
+  path: '/icons/postman.svg',
+  name: 'Postman',
+};
+const solidity = {
+  path: '/icons/solidity.svg',
+  name: 'Solidity',
+};
+const ethereum = {
+  path: '/icons/ethereum.svg',
+  name: 'Ethereum',
+};
+
+export default {
+  html,
+  css,
+  js,
+  mui,
+  react,
+  ts,
+  git,
+  figma,
+  nextjs,
+  nodejs,
+  express,
+  tailwind,
+  mongodb,
+  firebase,
+  postman,
+  solidity,
+  ethereum,
+};
