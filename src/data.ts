ceadbc6e2b4eb3bb940b585118c7bd5d4735dc2f
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-29 20:14:02 +0530

    chore: add project - CryptoNix

--- src/data.ts
+++ src/data.ts
@@ -5,11 +5,19 @@ export const projects = [
     description:
       'A web application platform to conduct online quizzing contests built with Next.JS, TypeScript & Express.js',
     imgSrc: '/chimerax.png',
-    liveLink: 'https://chimerax22-ui.vercel.app/',
+    liveLink: 'https://chimerax22.vercel.app/',
     repoLink: 'https://github.com/ISTE-SC-MANIT/chimerax22-ui',
   },
   {
     id: 2,
+    title: 'CryptoNix',
+    description: 'A NFT marketplace running on Ethereum with Polygon & Next.js',
+    imgSrc: '/cryptonix.png',
+    liveLink: 'https://cryptonix.vercel.app/',
+    repoLink: 'https://github.com/faisaljamil25/CryptoNix',
+  },
+  {
+    id: 3,
     title: 'Memories',
     description: 'Image sharing web app built with React and Express.js',
     imgSrc: '/memories.png',
@@ -17,7 +25,7 @@ export const projects = [
     repoLink: 'https://github.com/faisaljamil25/memories-client',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Ignite Games',
     description: 'Website to search video games information',
     imgSrc: '/ignite.png',
@@ -25,8 +33,8 @@ export const projects = [
     repoLink: 'https://github.com/faisaljamil25/ignite',
   },
   {
-    id: 4,
-    title: 'WAVES MUSIC PLAYER',
+    id: 5,
+    title: 'Waves Music Player',
     description: 'A music player built with React.js',
     imgSrc: '/music-player.png',
     liveLink: 'https://waves-musics-player.netlify.app/',
